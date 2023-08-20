a80e1ea0e3b0c3cc0d9292f600bf46d3dca286df
Phillip Marques <phillipmarq@hotmail.com>
2023-08-20 15:26:11 -0300

    Implementing methods for validating numbers and mathematical operations

--- 05_RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/CalculatorController.cs
+++ 05_RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/CalculatorController.cs
@@ -14,7 +14,7 @@ public class CalculatorController : ControllerBase
     }
 
     [HttpGet("sum/{firstNumber}/{secondNumber}")]
-    public IActionResult Get(string firstNumber, string secondNumber) 
+    public IActionResult Sum(string firstNumber, string secondNumber) 
     {
         if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
         {
@@ -25,13 +25,82 @@ public class CalculatorController : ControllerBase
         return BadRequest("Invalid Input");
     }
 
-    private int ConvertToDecimal(string firstNumber)
+    [HttpGet("subtraction/{firstNumber}/{secondNumber}")]
+    public IActionResult Subtraction(string firstNumber, string secondNumber)
     {
-        throw new NotImplementedException();
+        if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+        {
+            var subtraction = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+            return Ok(subtraction.ToString());
+        }
+
+        return BadRequest("Invalid Input");
     }
 
-    private bool IsNumeric(string firstNumber)
+    [HttpGet("multiplication/{firstNumber}/{secondNumber}")]
+    public IActionResult Multiplication(string firstNumber, string secondNumber)
     {
-        throw new NotImplementedException();
+        if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+        {
+            var multiplication = ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber);
+            return Ok(multiplication.ToString());
+        }
+
+        return BadRequest("Invalid Input");
     }
+
+    [HttpGet("division/{firstNumber}/{secondNumber}")]
+    public IActionResult Division(string firstNumber, string secondNumber)
+    {
+        if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+        {
+            var division = ConvertToDecimal(firstNumber)/ConvertToDecimal(secondNumber);
+            return Ok(division.ToString());
+        }
+
+        return BadRequest("Invalid Input");
+    }
+
+    [HttpGet("mean/{firstNumber}/{secondNumber}")]
+    public IActionResult Mean(string firstNumber, string secondNumber)
+    {
+        if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+        {
+            var mean = (ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber)) / 2;
+            return Ok(mean.ToString());
+        }
+
+        return BadRequest("Invalid Input");
+    }
+
+    [HttpGet("square-root/{firstNumber}")]
+    public IActionResult SquareRoot(string firstNumber)
+    {
+        if (IsNumeric(firstNumber))
+        {
+            var squareRoot = Math.Sqrt((double)ConvertToDecimal(firstNumber));
+            return Ok(squareRoot.ToString());
+        }
+
+        return BadRequest("Invalid Input");
+    }
+
+    private bool IsNumeric(string strNumber)
+    {
+        double number;
+        bool isNumber = double.TryParse(strNumber, 
+            System.Globalization.NumberStyles.Any, 
+            System.Globalization.NumberFormatInfo.InvariantInfo, 
+            out number);
+        return isNumber;
+    }
+
+    private decimal ConvertToDecimal(string strNumber)
+    {
+        decimal decimalValue;
+        if (decimal.TryParse(strNumber, out decimalValue))
+            return decimalValue;
+        return 0;
+    }
+        
 }
