ce1c71c254af969f626b956f694cd799f2460a8f
Phillip Marques <phillipmarq@hotmail.com>
2023-08-22 07:41:10 -0300

    Implementing the PostAsJson and PutAsJson methods in HttpClientExtensions

--- GeekShopping/GeekShopping.Web/Utils/HttpClientExtensions.cs
+++ GeekShopping/GeekShopping.Web/Utils/HttpClientExtensions.cs
@@ -1,9 +1,13 @@
-﻿using System.Text.Json;
+﻿using Microsoft.AspNetCore.Mvc.Formatters;
+using System.Net.Http.Headers;
+using System.Text.Json;
 
 namespace GeekShopping.Web.Utils
 {
     public static class HttpClientExtensions
     {
+        private static MediaTypeHeaderValue contentType 
+            = new MediaTypeHeaderValue("application/json");
         public static async Task<T> ReadContentAs<T>(
             this HttpResponseMessage response)
         {
@@ -15,6 +19,32 @@ namespace GeekShopping.Web.Utils
             return JsonSerializer.Deserialize<T>(dataAsString, 
                 new JsonSerializerOptions 
                 { PropertyNameCaseInsensitive = true });
+        }
+
+        public static Task<HttpResponseMessage> PostAsJson<T>(
+            this HttpClient httpClient,
+            string url,
+            T data)
+        {
+            var dataAsString = JsonSerializer.Serialize(data);
+            var content = new StringContent(dataAsString);
+
+            content.Headers.ContentType = contentType;
+
+            return httpClient.PostAsync(url, content);
+        }
+
+        public static Task<HttpResponseMessage> PutAsJson<T>(
+           this HttpClient httpClient,
+           string url,
+           T data)
+        {
+            var dataAsString = JsonSerializer.Serialize(data);
+            var content = new StringContent(dataAsString);
+
+            content.Headers.ContentType = contentType;
+
+            return httpClient.PutAsync(url, content);
         }
     }
 }
