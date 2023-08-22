24c384be80905588cedcc7e35b95a9f6674fb681
Phillip Marques <phillipmarq@hotmail.com>
2023-08-22 07:36:06 -0300

    Implementing the HttpClientExtensions Class

---
+++ GeekShopping/GeekShopping.Web/Utils/HttpClientExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+
+namespace GeekShopping.Web.Utils
+{
+    public static class HttpClientExtensions
+    {
+        public static async Task<T> ReadContentAs<T>(
+            this HttpResponseMessage response)
+        {
+            if (!response.IsSuccessStatusCode) throw
+                    new ApplicationException($"Something wnet wrong caling the API: " +
+                    $"{response.ReasonPhrase}");
+
+            var dataAsString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            return JsonSerializer.Deserialize<T>(dataAsString, 
+                new JsonSerializerOptions 
+                { PropertyNameCaseInsensitive = true });
+        }
+    }
+}
