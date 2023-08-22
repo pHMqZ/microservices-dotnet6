71901ee41860f94063ed52d553146fc7d71d74d1
Phillip Marques <phillipmarq@hotmail.com>
2023-08-22 08:47:11 -0300

    Product Controller implemented

---
+++ GeekShopping/GeekShopping.Web/Controllers/PrroductController.cs
@@ -0,0 +1,21 @@
+﻿using GeekShopping.Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.Web.Controllers
+{
+    public class PrroductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public PrroductController(IProductService productService)
+        {
+            _productService = productService ?? throw new ArgumentNullException(nameof(productService));
+        }
+
+        public async Task<IActionResult> ProductIndex()
+        {
+            var products = _productService.FindAllProducts();
+            return View(products);
+        }
+    }
+}
