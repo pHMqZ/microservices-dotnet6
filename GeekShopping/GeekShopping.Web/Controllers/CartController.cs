106050ec0a0acf7bcefa0a9fab2b5ea9a1c3908a
Phillip Marques <phillipmarq@hotmail.com>
2023-09-19 16:41:17 -0300

    Creating a method responsible for searching the user's shopping cart

--- GeekShopping/GeekShopping.Web/Controllers/CartController.cs
+++ GeekShopping/GeekShopping.Web/Controllers/CartController.cs
@@ -1,4 +1,6 @@
-﻿using GeekShopping.Web.Services.IServices;
+﻿using GeekShopping.Web.Models;
+using GeekShopping.Web.Services.IServices;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GeekShopping.Web.Controllers
@@ -15,9 +17,28 @@ namespace GeekShopping.Web.Controllers
             _cartService = cartService;
         }
 
-        public async Task<IActionResult>  CartIndex()
+        public async Task<IActionResult> CartIndex()
         {
-            return View();
+
+            return View(await FindUserCart());
+        }
+
+
+        private async Task<CartViewModel> FindUserCart()
+        {
+            var token = await HttpContext.GetTokenAsync("access_token");
+            var userId = User.Claims.Where(u => u.Type == "sub")?.FirstOrDefault()?.Value;
+
+            var response = await _cartService.FindCartByUserId(userId, token);
+            if (response?.CartHeader != null)
+            {
+                foreach (var detail in response.Details)
+                {
+                    response.CartHeader.PurchaseAmount += (detail.Product.Price * detail.Count);
+                }
+            }
+
+            return response;
         }
     }
 }
