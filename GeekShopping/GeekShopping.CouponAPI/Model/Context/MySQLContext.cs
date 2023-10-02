2543da390df91e32e53e832d2849b0ed77b74a2b
Phillip Marques <phillipmarq@hotmail.com>
2023-10-02 11:23:31 -0300

    Implementing repository classes

--- GeekShopping/GeekShopping.CouponAPI/Model/Context/MySQLContext.cs
+++ GeekShopping/GeekShopping.CouponAPI/Model/Context/MySQLContext.cs
@@ -8,6 +8,23 @@ namespace GeekShopping.CouponAPI.Model.Context
 
         public DbSet<Coupon> Coupons { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
 
+
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                Id = 1,
+                CouponCode = "PHMQZ_2023_10",
+                DiscountAmount = 10
+            });
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                Id = 2,
+                CouponCode = "PHMQZ_2023_15",
+                DiscountAmount = 15
+            });
+        }
     }
 }
