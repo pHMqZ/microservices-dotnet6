4fc1346a1658159a4c5abf053a98a727aab7d6e5
Phillip Marques <phillipmarq@hotmail.com>
2023-11-08 14:05:30 -0300

    Implementing push messages for the payment queue

--- GeekShopping/GeekShopping.OrderAPI/Program.cs
+++ GeekShopping/GeekShopping.OrderAPI/Program.cs
@@ -1,3 +1,4 @@
+using GeekShopping.GeekShopping.OrderAPI.RabbitMQSender;
 using GeekShopping.OrderAPI.MessageConsumer;
 using GeekShopping.OrderAPI.Model.Context;
 using GeekShopping.OrderAPI.RabbitMQSender;
@@ -27,7 +28,7 @@ builder.Services.AddSingleton(new OrderRepository(dbContextBuilder.Options));
 
 builder.Services.AddHostedService<RabbitMQCheckoutConsumer>();
 
-builder.Services.AddSingleton<IRabbitMQMessageSender, IRabbitMQMessageSender>();
+builder.Services.AddSingleton<IRabbitMQMessageSender, RabbitMQMessageSender>();
 
 builder.Services.AddControllers();
 
