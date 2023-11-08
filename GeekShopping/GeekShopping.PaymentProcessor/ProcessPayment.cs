21bfe0b97dc62f119a8a58f22a8e6d596a364450
Phillip Marques <phillipmarq@hotmail.com>
2023-11-08 13:17:03 -0300

    Creation of the Class Library responsible for processing payments

---
+++ GeekShopping/GeekShopping.PaymentProcessor/ProcessPayment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeekShopping.PaymentProcessor
+{
+    internal class ProcessPayment : IProcessPayment
+    {
+        public bool PaymentProcessor()
+        {
+            return true;
+        }
+    }
+}
