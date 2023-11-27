18b2ba37b0811a1052339b449a0f51a8eddcdc14
Phillip Marques <phillipmarq@hotmail.com>
2023-11-27 10:36:40 -0300

    Implementing Payment Consumer

--- GeekShopping/GeekShopping.PaymentAPI/Program.cs
+++ GeekShopping/GeekShopping.PaymentAPI/Program.cs
@@ -1,10 +1,16 @@
+using GeekShopping.PaymentAPI.MessageConsumer;
+using GeekShopping.PaymentProcessor;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddSingleton<IProcessPayment, ProcessPayment>();
+builder.Services.AddHostedService<RabbitMQPaymentConsumer>();
+builder.Services.AddControllers();
+
 builder.Services.AddAuthentication("Bearer")
     .AddJwtBearer("Bearer", options =>
     {
--- GeekShopping/GeekShopping.PaymentProcessor/ProcessPayment.cs
+++ GeekShopping/GeekShopping.PaymentProcessor/ProcessPayment.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace GeekShopping.PaymentProcessor
 {
-    internal class ProcessPayment : IProcessPayment
+    public class ProcessPayment : IProcessPayment
     {
         public bool PaymentProcessor()
         {
