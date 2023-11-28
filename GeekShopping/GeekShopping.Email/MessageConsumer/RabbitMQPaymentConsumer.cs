16a206856b829e67fe8ba973485e489506f11459
Phillip Marques <79226575+pHMqZ@users.noreply.github.com>
2023-11-28 15:31:45 -0300

    Working with Exchanges on RabbitMQ (#18)
    * Refactoring RabbitMQMessageSender (PaymentAPI) to send Fanout exchanges
    
    * Testing the sending of Fanout Exchanges
    
    * Refactoring OrderAPI to consume Exchange
    
    * Creating Email microservices
    
    * Implementing Messages, Entities and Context
    
    * Implementing Repositories
    
    * Implementing RabbitMQPaymentConsumer in Email
    
    * Implementing Message Sending and Consuming via Direct Exchanges

--- GeekShopping/GeekShopping.Email/MessageConsumer/RabbitMQPaymentConsumer.cs
+++ GeekShopping/GeekShopping.Email/MessageConsumer/RabbitMQPaymentConsumer.cs
@@ -1,64 +1,57 @@
-﻿using GeekShopping.OrderAPI.Messages;
-using GeekShopping.OrderAPI.Model;
-using GeekShopping.OrderAPI.RabbitMQSender;
-using GeekShopping.OrderAPI.Repository;
+﻿using GeekShopping.Email.Messages;
+using GeekShopping.Email.Repository;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System.Text;
 using System.Text.Json;
-using System.Threading.Channels;
 
-namespace GeekShopping.OrderAPI.MessageConsumer
+namespace GeekShopping.Email.MessageConsumer
 {
     public class RabbitMQPaymentConsumer : BackgroundService
     {
-        private readonly OrderRepository _repository;
+        private readonly EmailRepository _repository;
         private IConnection _connection;
         private IModel _channel;
         private const string ExchangeName = "DirectPaymentUpdateExchange";
-        private const string PaymentOrderUpdateQueueName = "PaymentOrderUpdateQueueName";
+        private const string PaymentEmailUpdateQueueName = "PaymentEmailUpdateQueueName";
 
-        public RabbitMQPaymentConsumer(OrderRepository repository)
+        public RabbitMQPaymentConsumer(EmailRepository repository)
         {
             _repository = repository;
-
             var factory = new ConnectionFactory
             {
                 HostName = "localhost",
                 UserName = "guest",
                 Password = "guest"
             };
-
             _connection = factory.CreateConnection();
             _channel = _connection.CreateModel();
 
             _channel.ExchangeDeclare(ExchangeName, ExchangeType.Direct);
-            _channel.QueueDeclare(PaymentOrderUpdateQueueName, false, false, false, null);
-            _channel.QueueBind(PaymentOrderUpdateQueueName, ExchangeName, "PaymentOrder");
+            _channel.QueueDeclare(PaymentEmailUpdateQueueName, false, false, false, null);
+            _channel.QueueBind(PaymentEmailUpdateQueueName, ExchangeName, "PaymentEmail");
         }
 
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
             stoppingToken.ThrowIfCancellationRequested();
-
             var consumer = new EventingBasicConsumer(_channel);
             consumer.Received += (chanel, evt) =>
             {
                 var content = Encoding.UTF8.GetString(evt.Body.ToArray());
-                UpdatePaymentResultVO vo = JsonSerializer.Deserialize<UpdatePaymentResultVO>(content);
-                UpdatePaymentStatus(vo).GetAwaiter().GetResult();
+                UpdatePaymentResultMessage message = JsonSerializer.Deserialize<UpdatePaymentResultMessage>(content);
+                ProcessLogs(message).GetAwaiter().GetResult();
                 _channel.BasicAck(evt.DeliveryTag, false);
             };
-            _channel.BasicConsume(PaymentOrderUpdateQueueName, false, consumer);
-
+            _channel.BasicConsume(PaymentEmailUpdateQueueName, false, consumer);
             return Task.CompletedTask;
         }
 
-        private async Task UpdatePaymentStatus(UpdatePaymentResultVO vo)
+        private async Task ProcessLogs(UpdatePaymentResultMessage message)
         {
             try
             {
-                await _repository.UpdateOrderPaymentStatus(vo.OrderId, vo.Status);
+                await _repository.LogEmail(message);
             }
             catch (Exception)
             {
---
+++ GeekShopping/GeekShopping.Email/Messages/UpdatePaymentResultMessage.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+namespace GeekShopping.Email.Messages
+{
+    public class UpdatePaymentResultMessage
+    {
+        public long OrderId { get; set; }
+        public bool Status { get; set; }
+        public string Email { get; set; }
+
+    }
+}
