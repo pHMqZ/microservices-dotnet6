099fd0b162a7862fa0788b9d060cefc878ffdfd0
Phillip Marques <phillipmarq@hotmail.com>
2023-11-28 13:09:15 -0300

    Implementing Messages, Entities and Context

---
+++ GeekShopping/GeekShopping.Email/Repository/EmailRepository.cs
@@ -0,0 +1,21 @@
+﻿using GeekShopping.Email.Messages;
+using GeekShopping.Email.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.Email.Repository
+{
+    public class EmailRepository : IEmailRepository
+    {
+        private readonly DbContextOptions<MySQLContext> _context;
+
+        public EmailRepository(DbContextOptions<MySQLContext> context)
+        {
+            _context = context;
+        }
+
+        public Task LogEmail(UpdatePaymentResultMessage message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
